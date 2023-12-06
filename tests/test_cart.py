2885b186b303c338412076e8f64c682b7bf7f6bc
KC Crouch <kody.crouch@aeropay.com>
2023-12-06 02:47:50 -0600

    stubs for tests

--- tests/test_cart.py
+++ tests/test_cart.py
@@ -1,41 +1,19 @@
-
-from demo_software_testing import create_cart, read_cart, create_product, add_to_cart, increment_quantity, decrement_quantity
+import pytest
 
 
 def test_create_cart():
-    cart = create_cart()
-    assert cart is not None
-    assert cart.id is not None
-    assert cart.empty
+    raise NotImplementedError
+
+def test_create_product():
+    raise NotImplementedError
 
 def test_add_to_cart():
-    cart = create_cart()
-    product = create_product()
-    add_to_cart(cart, product, 1)
-    cart = read_cart(cart.id)
-    assert not cart.empy
-    assert len(cart.products) == 1
-    assert cart.products[0].product_id == product.id
-    assert cart.products[0].quantity == 1
+    raise NotImplementedError
 
 
 def test_increment_quantity():
-    cart = create_cart()
-    product = create_product()
-    add_to_cart(cart, product, 1)
-    increment_quantity(cart, product)
-    cart = read_cart(cart.id)
-    assert cart.products[0].quantity == 2
+    raise NotImplementedError
+
 
 def test_decrement_quantity():
-    cart = create_cart()
-    product = create_product()
-    add_to_cart(cart, product, 2)
-    cart = read_cart(cart.id)
-    assert cart.products[0].quantity == 2
-    decrement_quantity(cart, product)
-    cart = read_cart(cart.id)
-    assert cart.products[0].quantity == 1
-    decrement_quantity(cart, product)
-    cart = read_cart(cart.id)
-    assert cart.empty
+    raise NotImplementedError
