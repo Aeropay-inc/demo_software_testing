354b14e5e3766aa769da73d7440fbe5681ee94d0
KC Crouch <kody.crouch@aeropay.com>
2023-12-06 07:21:56 -0600

    write the first tests

--- tests/test_cart.py
+++ tests/test_cart.py
@@ -1,21 +1,35 @@
-import pytest
+
+from demo_software_testing import *
 
 
 def test_create_cart():
-    raise NotImplementedError
-
+    cart = create_cart()
+    assert cart is not None
+    assert cart.id is not None
+    assert cart.empty
 
 def test_create_product():
-    raise NotImplementedError
-
-
-def test_add_to_cart():
-    raise NotImplementedError
-
+    product = create_product()
+    assert product is not None
+    assert product.id is not None
 
 def test_increment_quantity():
-    raise NotImplementedError
-
+    cart = create_cart()
+    product = create_product()
+    add_to_cart(cart, product, 1)
+    increment_quantity(cart, product)
+    cart = read_cart(cart.id)
+    assert cart.products[0].quantity == 2
 
 def test_decrement_quantity():
-    raise NotImplementedError
+    cart = create_cart()
+    product = create_product()
+    add_to_cart(cart, product, 2)
+    cart = read_cart(cart.id)
+    assert cart.products[0].quantity == 2
+    decrement_quantity(cart, product)
+    cart = read_cart(cart.id)
+    assert cart.products[0].quantity == 1
+    decrement_quantity(cart, product)
+    cart = read_cart(cart.id)
+    assert cart.empty
