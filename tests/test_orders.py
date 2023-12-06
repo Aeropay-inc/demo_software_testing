7d7dc76771d7848989e8d85577900b51a4df167f
KC Crouch <kody.crouch@aeropay.com>
2023-12-06 02:26:50 -0600

    test sanity

--- tests/test_orders.py
+++
@@ -1,5 +0,0 @@
-
-def test_my_sanity():
-    """
-    Write a test that simply makes an assertion you know is always true.
-    """
---
+++ tests/test_sanity.py
@@ -0,0 +1,7 @@
+
+def test_my_sanity():
+    """
+    Test that the tests are running correctly
+    """
+    assert True
+    assert not False
